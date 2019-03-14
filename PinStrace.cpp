c0f951cb1e88cd4d87b44af2ca75966c361e2ed4
Daniel Grumberg <dany.grumberg@gmail.com>
2019-03-14 13:53:59 +0000

    Set up instrumentation function and output machinery

--- PinStrace.cpp
+++ PinStrace.cpp
@@ -1,32 +1,46 @@
-
 /*! @file
  *  This is an example of the PIN tool that demonstrates some basic PIN APIs
  *  and could serve as the starting point for developing your first PIN tool
  */
 
 #include "pin.H"
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-/* ================================================================== */
-// Global variables
-/* ================================================================== */
+namespace {
+struct OStreamWrapper {
+public:
+  OStreamWrapper() : _Out(&std::clog), _IsFileStream(false) {}
+  OStreamWrapper(std::string &FileName) : _IsFileStream(true) {
+    _Out = new std::ofstream(FileName.c_str());
+  }
 
-UINT64 insCount = 0;        //number of dynamically executed instructions
-UINT64 bblCount = 0;        //number of dynamically executed basic blocks
-UINT64 threadCount = 0;     //total number of threads, including main thread
+  // Shallow is fine as we only delete in Fini
+  OStreamWrapper(const OStreamWrapper &Other) = default;
+  OStreamWrapper &operator=(const OStreamWrapper &Other) = default;
 
-std::ostream * out = &cerr;
+  ~OStreamWrapper() {
+    *_Out << "\n";
+    _Out->flush();
+    if (_IsFileStream) {
+      std::ofstream *FileOut = static_cast<std::ofstream *>(_Out);
+      FileOut->close();
+      delete FileOut;
+    }
+  }
+  std::ostream &getStream() { return *_Out; }
+
+private:
+  std::ostream *_Out;
+  bool _IsFileStream;
+};
+} // namespace
 
 /* ===================================================================== */
 // Command line switches
 /* ===================================================================== */
-KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE,  "pintool",
-    "o", "", "specify file name for MyPinTool output");
-
-KNOB<BOOL>   KnobCount(KNOB_MODE_WRITEONCE,  "pintool",
-    "count", "1", "count instructions, basic blocks and threads in the application");
-
+KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "",
+                            "specify file name for MyPinTool output");
 
 /* ===================================================================== */
 // Utilities
@@ -35,130 +49,86 @@ KNOB<BOOL>   KnobCount(KNOB_MODE_WRITEONCE,  "pintool",
 /*!
  *  Print out help message.
  */
-INT32 Usage()
-{
-    cerr << "This tool prints out the number of dynamically executed " << endl <<
-            "instructions, basic blocks and threads in the application." << endl << endl;
+static INT32 usage() {
+  cerr << "This tool implements strace via DBI with Intel Pin" << endl;
 
-    cerr << KNOB_BASE::StringKnobSummary() << endl;
+  cerr << KNOB_BASE::StringKnobSummary() << endl;
 
-    return -1;
+  return -1;
 }
 
 /* ===================================================================== */
 // Analysis routines
 /* ===================================================================== */
 
-/*!
- * Increase counter of the executed basic blocks and instructions.
- * This function is called for every basic block when it is about to be executed.
- * @param[in]   numInstInBbl    number of instructions in the basic block
- * @note use atomic operations for multi-threaded applications
- */
-VOID CountBbl(UINT32 numInstInBbl)
-{
-    bblCount++;
-    insCount += numInstInBbl;
+static VOID SysBefore(std::ostream &Output, ADDRINT Ip, ADDRINT Nr,
+                      ADDRINT Arg1, ADDRINT Arg2, ADDRINT Arg3, ADDRINT Arg4,
+                      ADDRINT Arg5, ADDRINT Arg6) {
+  Output << "In ";
+}
+
+static VOID SysAfter(std::ostream &Output, ADDRINT Ret) {
+  Output <<"and out.\n";
 }
 
 /* ===================================================================== */
 // Instrumentation callbacks
 /* ===================================================================== */
 
-/*!
- * Insert call to the CountBbl() analysis routine before every basic block
- * of the trace.
- * This function is called every time a new trace is encountered.
- * @param[in]   trace    trace to be instrumented
- * @param[in]   v        value specified by the tool in the TRACE_AddInstrumentFunction
- *                       function call
- */
-VOID Trace(TRACE trace, VOID *v)
-{
-    // Visit every basic block in the trace
-    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
-    {
-        // Insert a call to CountBbl() before every basic bloc, passing the number of instructions
-        BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)CountBbl, IARG_UINT32, BBL_NumIns(bbl), IARG_END);
-    }
+static VOID SyscallEntry(THREADID ThreadIndex, CONTEXT *Context,
+                         SYSCALL_STANDARD Std, VOID *V) {
+  OStreamWrapper *Output = static_cast<OStreamWrapper *>(V);
+  SysBefore(Output->getStream(), PIN_GetContextReg(Context, REG_INST_PTR),
+            PIN_GetSyscallNumber(Context, Std),
+            PIN_GetSyscallArgument(Context, Std, 0),
+            PIN_GetSyscallArgument(Context, Std, 1),
+            PIN_GetSyscallArgument(Context, Std, 2),
+            PIN_GetSyscallArgument(Context, Std, 3),
+            PIN_GetSyscallArgument(Context, Std, 4),
+            PIN_GetSyscallArgument(Context, Std, 5));
 }
 
-/*!
- * Increase counter of threads in the application.
- * This function is called for every thread created by the application when it is
- * about to start running (including the root thread).
- * @param[in]   threadIndex     ID assigned by PIN to the new thread
- * @param[in]   ctxt            initial register state for the new thread
- * @param[in]   flags           thread creation flags (OS specific)
- * @param[in]   v               value specified by the tool in the
- *                              PIN_AddThreadStartFunction function call
- */
-VOID ThreadStart(THREADID threadIndex, CONTEXT *ctxt, INT32 flags, VOID *v)
-{
-    threadCount++;
+static VOID SyscallExit(THREADID ThreadIndex, CONTEXT *Context,
+                        SYSCALL_STANDARD Std, VOID *V) {
+  OStreamWrapper *Output = static_cast<OStreamWrapper *>(V);
+  SysAfter(Output->getStream(), PIN_GetSyscallReturn(Context, Std));
 }
 
-/*!
- * Print out analysis results.
- * This function is called when the application exits.
- * @param[in]   code            exit code of the application
- * @param[in]   v               value specified by the tool in the
- *                              PIN_AddFiniFunction function call
- */
-VOID Fini(INT32 code, VOID *v)
-{
-    *out <<  "===============================================" << endl;
-    *out <<  "MyPinTool analysis results: " << endl;
-    *out <<  "Number of instructions: " << insCount  << endl;
-    *out <<  "Number of basic blocks: " << bblCount  << endl;
-    *out <<  "Number of threads: " << threadCount  << endl;
-    *out <<  "===============================================" << endl;
+static VOID Fini(INT32 Code, VOID *V) {
+  OStreamWrapper *Output = static_cast<OStreamWrapper *>(V);
+  delete Output;
 }
 
-/*!
- * The main procedure of the tool.
- * This function is called when the application image is loaded but not yet started.
- * @param[in]   argc            total number of elements in the argv array
- * @param[in]   argv            array of command line arguments,
- *                              including pin -t <toolname> -- ...
- */
-int main(int argc, char *argv[])
-{
-    // Initialize PIN library. Print help message if -h(elp) is specified
-    // in the command line or the command line is invalid
-    if( PIN_Init(argc,argv) )
-    {
-        return Usage();
-    }
+/* ===================================================================== */
+// Main procedure
+/* ===================================================================== */
 
-    string fileName = KnobOutputFile.Value();
+int main(int argc, char *argv[]) {
+  // Initialize PIN library. Print help message if -h(elp) is specified
+  // in the command line or the command line is invalid
+  if (PIN_Init(argc, argv))
+    return usage();
 
-    if (!fileName.empty()) { out = new std::ofstream(fileName.c_str());}
+  string FileName = KnobOutputFile.Value();
 
-    if (KnobCount)
-    {
-        // Register function to be called to instrument traces
-        TRACE_AddInstrumentFunction(Trace, 0);
+  OStreamWrapper *Output =
+      FileName.empty() ? new OStreamWrapper() : new OStreamWrapper(FileName);
 
-        // Register function to be called for every thread before it starts running
-        PIN_AddThreadStartFunction(ThreadStart, 0);
+  cerr << "===============================================" << endl;
+  cerr << "This application is instrumented by PinStace" << endl;
+  if (!KnobOutputFile.Value().empty()) {
+    cerr << "See file " << KnobOutputFile.Value() << " for analysis results"
+         << endl;
+  }
+  cerr << "===============================================" << endl;
 
-        // Register function to be called when the application exits
-        PIN_AddFiniFunction(Fini, 0);
-    }
+  PIN_AddSyscallEntryFunction(SyscallEntry, (VOID *)Output);
+  PIN_AddSyscallExitFunction(SyscallExit, (VOID *)Output);
+  PIN_AddFiniFunction(Fini, (VOID *)Output);
+  // Start the program, never returns
+  PIN_StartProgram();
 
-    cerr <<  "===============================================" << endl;
-    cerr <<  "This application is instrumented by MyPinTool" << endl;
-    if (!KnobOutputFile.Value().empty())
-    {
-        cerr << "See file " << KnobOutputFile.Value() << " for analysis results" << endl;
-    }
-    cerr <<  "===============================================" << endl;
-
-    // Start the program, never returns
-    PIN_StartProgram();
-
-    return 0;
+  return 0;
 }
 
 /* ===================================================================== */
